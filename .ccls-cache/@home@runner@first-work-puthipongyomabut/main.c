2b6265d0affb5bc6cf4346ad7dbd9abdf7b9d92f
puthipongyomabu <123081303+puthipongyomabut@users.noreply.github.com>
2023-01-27 07:47:11 +0000

    assg1

--- .ccls-cache/@home@runner@first-work-puthipongyomabut/main.c
+++ .ccls-cache/@home@runner@first-work-puthipongyomabut/main.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char * argv[]) {
     
-    printf("argc is %d \n",argc);
+    printf("argc is %d\n",argc);
     int i,N;
     for(i=1;i<argc;i++){
         printf("%s\n" ,argv[i]);
